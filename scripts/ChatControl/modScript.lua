ba5acd096164d975eadfeb941d54b93735611c98
Tyler Hoyt <tylerhoyt93@gmail.com>
2024-10-14 14:36:23 -0400

    Rename everything to ChatNG in code

--- scripts/ChatControl/modScript.lua
+++
@@ -1,2 +0,0 @@
-load("ChatControl") -- Load the mod
-setExtensionUnloadMode("ChatControl", "manual") -- Set the unload mode to manual (This is very new, old function is deprecated)
---
+++ scripts/ChatNG/modScript.lua
@@ -0,0 +1,2 @@
+load("ChatNG") -- Load the mod
+setExtensionUnloadMode("ChatNG", "manual") -- Set the unload mode to manual (This is very new, old function is deprecated)
